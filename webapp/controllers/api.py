d9670d61de8a95051f32bddaa0bcea76f2da3f54
Serge Zolotukhin <serge.zolotukhin@gmail.com>
2018-06-07 12:08:31 +0300

    Removed @tools.auth() from methods that used by Engine. Bad parames and data not found test implemented.

--- webapp/controllers/api.py
+++ webapp/controllers/api.py
@@ -8,133 +8,165 @@ from webapp.libs.models.calculation import \
     Data as DataModel
 
 
-__all__ = ["Api"]
+__all__ = ['Api']
 
 
 class Api(object):
 
-    @cherrypy.expose(["run-graph"])
+    ERR_OK = 0
+    ERR_BAD_PARAMS = 1
+    ERR_DATA_NOT_FOUND = 2
+    ERR_REMOTE_HOST_SCRIPT_ERROR = 4
+    ERR_REMOTE_HOST_HTTP_ERROR = 8
+    ERR_REMOTE_HOST_UNREACHABLE = 16
+
+    @cherrypy.expose(['run-graph'])
     @cherrypy.tools.json_in()
     @cherrypy.tools.json_out()
     @cherrypy.tools.auth()
     def run_graph(self):
         """ RUN """
-        calculation_id = cherrypy.request.json["calculation_id"]
-        title = cherrypy.request.json["title"]
-        params = cherrypy.request.json["params"]
+        calculation_id = cherrypy.request.json['calculation_id']
+        title = cherrypy.request.json['title']
+        params = cherrypy.request.json['params']
 
         # TODO: check params
 
         sess = cherrypy.request.sa
 
         # Кладем данные в БД
-        graph = GraphModel(calculation_id, title, str(params).replace("'", "\""))
+        graph = GraphModel(calculation_id, title, str(params).replace('"', '\''))
         sess.add(graph)
         sess.flush()
 
         # Запускаем процесс на стороне
-        ce_config = cherrypy.tree.apps[""].config["CalculationsEngine"]
-        ce_url = "http://%s:%s/" % (ce_config["ce.host"], ce_config["ce.port"])
+        ce_config = cherrypy.tree.apps[''].config['CalculationsEngine']
+        ce_url = 'http://%s:%s/' % (ce_config['ce.host'], ce_config['ce.port'])
         ce_params = {
-            "cmd": "start",
-            "calculation_id": calculation_id,
-            "graph_id": graph.id,
-            "params": params
+            'cmd': 'start',
+            'calculation_id': calculation_id,
+            'graph_id': graph.id,
+            'params': params
         }
-        err_code = 0
-        err_message = ""
+        error = self.ERR_OK
+        err_message = ''
         try:
             resp = requests.post(ce_url, json=ce_params)
             if resp.status_code == 200:
                 rest_json = json.loads(resp.text)
-                if "error" in rest_json and rest_json["error"] != 0:
-                    err_code = 3
-                    err_message = "Got error code (%s) from engine side." % rest_json["error"]
+                if 'error' in rest_json and rest_json['error'] != 0:
+                    error = self.ERR_REMOTE_HOST_SCRIPT_ERROR
+                    err_message = 'Got error code (%s) from engine side.' % rest_json['error']
             else:
-                # HTTP error while engine requesting
-                err_code = 2
-                err_message = "Got HTTP error %s" % resp.status_code
+                error = self.ERR_REMOTE_HOST_HTTP_ERROR
+                err_message = 'Got HTTP error %s' % resp.status_code
                 sess.delete(graph)
         except requests.exceptions.ConnectionError:
-            # System error while engine requesting
-            err_code = 1
-            err_message = "Cant connect to %s" % ce_url
+            error = self.ERR_REMOTE_HOST_UNREACHABLE
+            err_message = 'Cant connect to %s' % ce_url
             sess.delete(graph)
 
-        return {"error": err_code, "message": err_message}
+        return {'error': error, 'message': err_message}
 
-    @cherrypy.expose(["finish-graph"])
-    @cherrypy.tools.json_in()
-    @cherrypy.tools.json_out()
-    @cherrypy.tools.auth()
-    def finish_graph(self):
-        err_code = 1
-        if "graph_id" in cherrypy.request.json:
-            graph_id = cherrypy.request.json["graph_id"]
-            graph = GraphModel.get(cherrypy.request.sa, graph_id)
-            graph.updated = datetime.datetime.now()
-            graph.finished = True
-            err_code = 0
-        return {"error": err_code}
-
-    @cherrypy.expose(["delete-graph"])
+    @cherrypy.expose(['delete-graph'])
     @cherrypy.tools.json_in()
     @cherrypy.tools.json_out()
     @cherrypy.tools.auth()
     def delete_graph(self):
-        err_code = 1
-        if "graph_id" in cherrypy.request.json:
-            graph_id = cherrypy.request.json["graph_id"]
+        error = self.ERR_OK
+        if 'graph_id' in cherrypy.request.json:
             sess = cherrypy.request.sa
+            graph_id = cherrypy.request.json['graph_id']
             graph = GraphModel.get(sess, graph_id)
-            sess.delete(graph)
-            sess.commit()
-            err_code = 0
-        return {"error": err_code}
+            if graph:
+                sess.delete(graph)
+                sess.commit()
+            else:
+                error = self.ERR_DATA_NOT_FOUND
+        else:
+            error = self.ERR_BAD_PARAMS
+        return {'error': error}
 
-    @cherrypy.expose(["update-graph"])
-    @cherrypy.tools.json_in()
-    @cherrypy.tools.json_out()
-    @cherrypy.tools.auth()
-    def update_graph(self):
-        graph_id = cherrypy.request.json["graph_id"]
-        x = cherrypy.request.json["x"]
-        y = cherrypy.request.json["y"]
-
-        cherrypy.log(json.dumps(cherrypy.request.json))
-
-        sess = cherrypy.request.sa
-
-        # Data
-        data = DataModel(graph_id, x, y)
-        sess.add(data)
-        sess.flush()
-
-        updated = datetime.datetime.now()
-
-        # Graph
-        graph = GraphModel.get(sess, graph_id)
-        graph.updated = updated
-
-        # Calculation
-        calc = CalculationModel.get(sess, graph.calculation_id)
-        calc.updated = updated
-
-        return {"error": 0}
-
-    @cherrypy.expose(["create-calculation"])
+    @cherrypy.expose(['create-calculation'])
     @cherrypy.tools.json_in()
     @cherrypy.tools.json_out()
     @cherrypy.tools.auth()
     def create_calculation(self):
-        title = cherrypy.request.json["title"]
-        description = cherrypy.request.json["description"]
-        params_template = cherrypy.request.json["params_template"]
+        error = self.ERR_OK
+        if 'title' in cherrypy.request.json and \
+                'description' in cherrypy.request.json and \
+                'params_template' in cherrypy.request.json:
+            title = cherrypy.request.json['title']
+            description = cherrypy.request.json['description']
+            params_template = cherrypy.request.json['params_template']
+            sess = cherrypy.request.sa
+            calculation = CalculationModel(title, description, params_template)
+            sess.add(calculation)
+            sess.flush()
+        else:
+            error = self.ERR_BAD_PARAMS
 
-        sess = cherrypy.request.sa
+        return {'error': error}
 
-        calculation = CalculationModel(title, description, params_template)
-        sess.add(calculation)
-        sess.flush()
+    # **********************************************************************************************************
+    #
+    # Non secured methods coz of Engine doesnt support cookies
+    # and we cannot init session to store user token there
+    #
 
-        return {"error": 0}
+    # TODO: SOLVE THIS SOMEHOW!!!
+
+    @cherrypy.expose(['update-graph'])
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def update_graph(self):
+        """
+        Append point to graph data.
+        :return: Json with error code
+        """
+        error = self.ERR_OK
+        if 'graph_id' in cherrypy.request.json and \
+                'x' in cherrypy.request.json and \
+                'y' in cherrypy.request.json:
+
+            graph_id = cherrypy.request.json['graph_id']
+            sess = cherrypy.request.sa
+
+            # Test if there is graph with given id
+            graph = GraphModel.get(sess, graph_id)
+            if graph:
+                # Append data
+                x = cherrypy.request.json['x']
+                y = cherrypy.request.json['y']
+                data = DataModel(graph_id, x, y)
+                sess.add(data)
+                sess.flush()
+                # Update Graph and  Calculation
+                calc = CalculationModel.get(sess, graph.calculation_id)
+                calc.updated = graph.updated = datetime.datetime.now()
+            else:
+                error = self.ERR_DATA_NOT_FOUND
+        else:
+            error = self.ERR_BAD_PARAMS
+        return {'error': error}
+
+    @cherrypy.expose(['finish-graph'])
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def finish_graph(self):
+        """
+        Sets graph.finished flag to True value.
+        :return: Json with error code
+        """
+        error = self.ERR_OK
+        if 'graph_id' in cherrypy.request.json:
+            graph_id = cherrypy.request.json['graph_id']
+            graph = GraphModel.get(cherrypy.request.sa, graph_id)
+            if graph:
+                graph.updated = datetime.datetime.now()
+                graph.finished = True
+            else:
+                error = self.ERR_DATA_NOT_FOUND
+        else:
+            error = self.ERR_BAD_PARAMS
+        return {'error': error}
